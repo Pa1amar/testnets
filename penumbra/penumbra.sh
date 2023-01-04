6eca3464e1c52ebbc76bd19c53a449a1ebb7a0aa
Palamar <palamarsat@gmail.com>
2023-01-04 11:02:09 +0200

    Update penumbra.sh

--- penumbra/penumbra.sh
+++ penumbra/penumbra.sh
@@ -6,8 +6,8 @@ sudo apt install make clang pkg-config libssl-dev build-essential git jq ncdu bs
 
 echo -e '\n\e[42mInstall Go\e[0m\n' && sleep 1
 cd $HOME
-wget -O go1.19.2.linux-amd64.tar.gz https://golang.org/dl/go1.18.1.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.2.linux-amd64.tar.gz && rm go1.19.2.linux-amd64.tar.gz
+wget -O go1.18.1.linux-amd64.tar.gz https://golang.org/dl/go1.18.1.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.1.linux-amd64.tar.gz && rm go1.18.1.linux-amd64.tar.gz
 echo 'export GOROOT=/usr/local/go' >> $HOME/.bash_profile
 echo 'export GOPATH=$HOME/go' >> $HOME/.bash_profile
 echo 'export GO111MODULE=on' >> $HOME/.bash_profile
