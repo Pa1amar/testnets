3790b10dc3a94cb3b9bda3e8913363ce8dab128a
Palamar <palamarsat@gmail.com>
2023-02-22 08:13:09 +0200

    Update penumbra.sh

--- penumbra/penumbra.sh
+++ penumbra/penumbra.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt install make curl clang pkg-config libssl-dev build-essential git jq ncdu bsdmainutils -y < "/dev/null"
 
 IP_ADDRESS=$(curl ifconfig.me)
-PENUMBRA_BRANCH=044-ananke
+PENUMBRA_BRANCH=045-metis
 TENDERMINT_BRANCH=v0.34.23
 echo -e '\n\e[42mInstall Go\e[0m\n' && sleep 1
 cd $HOME
