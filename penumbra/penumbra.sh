360f695cc7551e3565f009db8bb7b1029ac27619
Palamar <palamarsat@gmail.com>
2023-03-15 13:34:20 +0200

    Update penumbra.sh

--- penumbra/penumbra.sh
+++ penumbra/penumbra.sh
@@ -2,7 +2,7 @@
 read -p "Enter your node name: " MONIKER
 sudo apt update && sudo apt install make curl clang pkg-config libssl-dev build-essential git jq ncdu bsdmainutils git-lfs -y < "/dev/null"
 IP_ADDRESS=$(curl ifconfig.me)
-PENUMBRA_BRANCH=048-carme
+PENUMBRA_BRANCH=048-carme.1
 TENDERMINT_BRANCH=v0.34.23
 echo -e '\n\e[42mInstall Go\e[0m\n' && sleep 1
 cd $HOME
