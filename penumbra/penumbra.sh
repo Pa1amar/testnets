9e735a6efc1dbf6b6eaddb093bff89c80958933e
Palamar <palamarsat@gmail.com>
2023-09-26 05:31:53 +0300

    Update penumbra.sh

--- penumbra/penumbra.sh
+++ penumbra/penumbra.sh
@@ -2,10 +2,10 @@
 read -p "Enter your node name: " MONIKER
 sudo apt update && sudo apt install make curl clang pkg-config libssl-dev build-essential git jq ncdu bsdmainutils git-lfs -y < "/dev/null"
 IP_ADDRESS=$(curl ipinfo.io/ip):26656
-PENUMBRA_BRANCH=v0.60.0
-TENDERMINT_BRANCH=v0.34.23
+PENUMBRA_BRANCH=v0.61.0
+COMEBFT_BRANCH=v0.34.27
 sudo systemctl stop penumbrad 2>/dev/null
-sudo systemctl stop tendermintd 2>/dev/null
+sudo systemctl stop cometbftd 2>/dev/null
 echo -e '\n\e[42mInstall Go\e[0m\n' && sleep 1
 cd $HOME
 wget -O go1.18.1.linux-amd64.tar.gz https://golang.org/dl/go1.18.1.linux-amd64.tar.gz
@@ -21,14 +21,14 @@ source $HOME/.cargo/env
 source $HOME/.bash_profile
 echo -e '\n\e[42mInstall software\e[0m\n' && sleep 1
 cd $HOME
-rm -rf $HOME/tendermint
-git clone https://github.com/tendermint/tendermint.git
-cd tendermint
-git checkout $TENDERMINT_BRANCH
+rm -rf $HOME/cometbft
+git clone git clone https://github.com/cometbft/cometbft.git
+cd cometbft
+git checkout $COMEBFT_BRANCH
 make install || exit
 sleep 2
-tendermint init full
-curl -s https://raw.githubusercontent.com/penumbra-zone/penumbra/main/testnets/tm_config_template.toml > $HOME/.tendermint/config/config.toml
+cometbft init full
+#curl -s https://raw.githubusercontent.com/penumbra-zone/penumbra/main/testnets/tm_config_template.toml > $HOME/.penumbra/testnet_data/node0/cometbft/config/config.toml
 sleep 2
 cd $HOME
 rm -rf $HOME/penumbra
@@ -46,19 +46,19 @@ rm -rf $HOME/.cargo/git/checkouts/*
 pd testnet unsafe-reset-all
 pcli view reset
 pd testnet join --external-address $IP_ADDRESS --moniker $MONIKER
-echo -e '\n\e[42mCreating a service for Tendermint Node ...\e[0m\n' && sleep 1
+echo -e '\n\e[42mCreating a service for Cometbft Node ...\e[0m\n' && sleep 1
 echo "[Unit]
-Description=Tendermint Node
+Description=Cometbft Node
 After=network-online.target
 [Service]
 User=$USER
-ExecStart=$(which tendermint) start --home "$HOME/.penumbra/testnet_data/node0/tendermint/" 
+ExecStart=/usr/local/bin/cometbft start --home "$HOME/.penumbra/testnet_data/node0/cometbft/" 
 Restart=always
 RestartSec=10
 LimitNOFILE=10000
 [Install]
 WantedBy=multi-user.target
-" > $HOME/tendermintd.service
+" > $HOME/cometbftd.service
 echo -e '\n\e[42mCreating a service for Penumbra Node...\e[0m\n' && sleep 1
 echo "[Unit]
 Description=Penumbra Node
@@ -72,16 +72,16 @@ LimitNOFILE=10000
 [Install]
 WantedBy=multi-user.target
 " > $HOME/penumbrad.service
-sudo mv $HOME/tendermintd.service /etc/systemd/system
+sudo mv $HOME/cometbftd.service /etc/systemd/system
 sudo mv $HOME/penumbrad.service /etc/systemd/system
 sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
 Storage=persistent
 EOF
 sudo systemctl restart systemd-journald
 sudo systemctl daemon-reload
-echo -e '\n\e[42mEnabling Tendermint and Penumbra Node services\e[0m\n' && sleep 1
-sudo systemctl enable tendermintd
+echo -e '\n\e[42mEnabling cometbft and Penumbra Node services\e[0m\n' && sleep 1
+sudo systemctl enable cometbftd
 sudo systemctl enable penumbrad
 sudo systemctl restart penumbrad
 sleep 15
-sudo systemctl restart tendermintd
+sudo systemctl restart cometbftd
